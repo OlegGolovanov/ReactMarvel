e1c945fe2a9254b25f24dca997d98dea1e164253
Oleg <olegjan9@rambler.ru>
2022-06-19 19:27:27 +0300

    19.06.2022

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
+import GetMarvelData from "./services/GetMarvelData";
+
+const NewGetMarvelData = new GetMarvelData();
+NewGetMarvelData.resPost().then(data => console.log(data));
 
 ReactDOM.render(
   <React.StrictMode>
