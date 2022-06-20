1d089d649a52ba26b5478a0ae4a51d14ac038437
Oleg <olegjan9@rambler.ru>
2022-06-20 08:35:09 +0300

    21.06.2022

--- src/services/GetMarvelData.js
+++ src/services/GetMarvelData.js
@@ -1,16 +1,21 @@
 class GetMarvelData{   
-    // address = 
-
-    post = async (url)=>{
-        
+    
+    _address = 'https://gateway.marvel.com:443/v1/public/';
+    
+    address = () => {
+        return this._address = 'https://gateway.marvel.com:443/v1/public/';
+    }
+    _apikey = 'apikey=827ef5444e9fbf654e8fa51f975d051a';
+    
+    post = async(url) => {        
         let res = await fetch(url);
         return await res.json();
     }
 
-    resPost = () => {
-     return this.post(`
-     https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=827ef5444e9fbf654e8fa51f975d051a`
-     )
+    resPostAllCharacter = () => {
+        return this.post(`
+        ${this.address()}characters?limit=9&offset=210&${this._apikey}`
+        )
     }
 
 
